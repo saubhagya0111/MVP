52cd14946deac421b13bb3d10176fc2040d10842
saubhagya0111 <chopragolu2345@gmail.com>
2024-06-09 12:45:40 +0530

    django server running

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'tweets',
+    'videos',
+    'uploads',
+    'rest_framework',
+    'social_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tweet_to_video',  # Database name
+        'USER': 'username',  # Your MySQL username
+        'PASSWORD': 'password',  # Your MySQL password
+        'HOST': 'localhost',  # Or the IP address of your MySQL server
+        'PORT': '3306',  # MySQL default port
     }
 }
 
--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name='custom_user_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set_permissions', blank=True)
+
+class OAuthToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    provider = models.CharField(max_length=50)
+    token = models.TextField()
+
+
